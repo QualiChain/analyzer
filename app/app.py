74f6ddec72491a769c9644dcab81827508fdd122
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-04-28 20:46:25 +0300

    some changes on app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import sys
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 
-from utils import check_input_data
+from utils import check_input_data, check_source
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -28,7 +28,17 @@ class ReceiveDataSource(Resource):
         if data_format:
             log.info('Data Formatted correctly')
 
-            return 201
+            _check = check_source(
+                input_uri=data['uri'],
+                type=data['type'],
+                part=data['part']
+            )
+            if _check:
+                response_msg = {'message': 'Data Source send for processing'}
+                return response_msg, 201
+            else:
+                response_msg = {'message': 'Invalid Data Source'}
+                return response_msg, 400
         else:
             log.error('Data malformed')
             response_msg = {'message': 'Your input must contain uri, data source type and part'}
